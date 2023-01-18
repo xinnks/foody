7502d1769de17dfa3ae30ec6bcbf96cd8528cd0d
James Sinkala <xinnks@gmail.com>
2023-01-18 15:59:21 +0300

    feat: Add nuxt configuration file, configure some modules, env variables, tailwind configuration, and initiate page meta data defaults

---
+++ nuxt.config.js
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      titleTemplate: '%s - Foody',
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'Eat the food you deserve.' }
+      ]
+    }
+  },
+  runtimeConfig: {
+    novuApiKey: '',
+    public: {
+      loginPageLink: '',
+      firstTimeLoginPageLink: '',
+      novuApiIdentifier: '',
+      passwordResetLink: '',
+      adminDomain: '',
+    },
+  },
+  modules: [
+    '@nuxtjs/supabase',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/supabase'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
