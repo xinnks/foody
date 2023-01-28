8742699effdc6c970357054a8619be4abed87ac9
James Sinkala <xinnks@gmail.com>
2023-01-28 07:13:41 +0300

    feat: Add a dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useCartStore } from "~~/store/cart";
+
+const cartStore = useCartStore();
+const { showCart } = storeToRefs(cartStore);
+const { toggleCart } = cartStore;
+
+function closeCart() {
+  if(showCart.value){
+    toggleCart()
+  }
+}
+</script>
+
+<template>
+  <div class="bg-light flex flex-col min-h-[100vh]">
+    <NavigationBar />
+    <main class="flex-1 flex flex-col w-full md:max-w-6xl mx-auto relative z-0" @click="closeCart()">
+        <slot></slot>
+    </main>
+    <Cart/>
+    <AppFooter/>
+  </div>
+</template>
