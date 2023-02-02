fea64fc9a6026879cc17990b3421c1e9319ca44c
James Sinkala <xinnks@gmail.com>
2023-02-02 12:37:48 +0300

    feat: Add admin dashboard layout component

---
+++ layouts/admin-dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-light flex flex-col min-h-[100vh]">
+    <NavigationBar>
+    </NavigationBar>
+    <main class="flex-1 flex flex-col w-full md:max-w-6xl mx-auto relative z-0">
+        <slot></slot>
+    </main>
+    <AppFooter/>
+  </div>
+</template>
