9e501d73c16cbd941b6c79f48b05738e6534413e
James Sinkala <xinnks@gmail.com>
2023-02-04 17:42:43 +0300

    feat: Add some public environmental variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,8 @@ export default defineNuxtConfig({
       novuApiIdentifier: '',
       passwordResetLink: '',
       adminDomain: '',
+      optOutLink: '',
+      feedbackUrl: ''
     },
   },
   modules: [
